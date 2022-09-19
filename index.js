866e366bec675575f11323c019b5e2bd69c082fa
vedantpawar18 <vedantpawar18@gmail.com>
2022-09-19 14:22:06 +0530

    app initialised

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const express= require("express")
+const app=express();
 
+app.get("/", (req,res)=>{
+    res.send("Welcome to the home page");
+})
+
+
+
+
+app.listen(8080, async()=>{
+    console.log("listening to port 8080")
+})
